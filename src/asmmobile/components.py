0dbf6f6b36d949f22ff767df14071b1ab9c12e0c
Jussi Judin <jjudin+bitbucket@iki.fi>
2009-09-18 20:04:41 +0300

    Removing constant time.

--- src/asmmobile/components.py
+++ src/asmmobile/components.py
@@ -48,9 +48,7 @@ class MobileView(grok.View):
             self.contentType
             )
 
-#         self.now = config.clock.now(dateutil.tz.tzlocal())
-        import datetime
-        self.now = datetime.datetime(2009, 8, 8, 18, tzinfo=dateutil.tz.tzlocal())
+        self.now = config.clock.now(dateutil.tz.tzlocal())
 
         utcnow = config.clock.now(dateutil.tz.tzutc())
         cacheTime = self.cacheTime(utcnow)
