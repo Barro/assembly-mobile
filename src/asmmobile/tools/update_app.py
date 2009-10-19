61e480455b160bb011bcccac2003082563546afc
Jussi Judin <jjudin+bitbucket@iki.fi>
2009-10-19 12:42:36 +0300

    Adding application for quick updates.

---
+++ src/asmmobile/tools/update_app.py
@@ -0,0 +1,34 @@
+import grok
+
+from asmmobile.tools.update_schedule import updateSchedule
+
+class UpdateScheduleApp(grok.Application):
+    pass
+
+
+class UpdateSchedule(grok.Model):
+    pass
+
+class Index(grok.View):
+    grok.context(UpdateSchedule)
+
+    def update(self):
+        asmmobile = __import__("asmmobile")
+        config = getattr(asmmobile.tools, 'import-config')
+        updateSchedule(self.context.__parent__, config)
+
+    def render(self):
+        return "OK"
+
+class BaseTraverser(grok.Traverser):
+    grok.context(UpdateScheduleApp)
+
+    def traverse(self, name):
+        site = grok.getSite()
+        root = site.__parent__
+        if name in root:
+            scheduleUpdater = UpdateSchedule()
+            scheduleUpdater.__name__ = name
+            scheduleUpdater.__parent__ = root[name]
+            return scheduleUpdater
+        return None
