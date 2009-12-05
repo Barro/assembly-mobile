62da04c399d76d9cb70c88b8f485be062c1399a0
Jussi Judin <jjudin+bitbucket@iki.fi>
2009-12-05 23:17:19 +0200

    Revert back to Assembly import configuration.

--- src/asmmobile/tools/import-config.py
+++ src/asmmobile/tools/import-config.py
@@ -17,11 +17,31 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-IMPORTERS = {'altparty': {
-        'filename': '/home/jussi/scratch/altparty-mobile/events.xml',
-        'prefix': 'altparty2009_'}
-             }
 
-MAPPING_LOCATIONS = {}
+IMPORTERS = {
+    'icalendar': {
+        'filename': '/tmp/asmtv.ics',
+        'locationMap': {'none': 'AssemblyTV'},
+        'majorCategory': 'Major_event',
+        },
+    'asmcsv': {
+        'filename': '/tmp/schedule.csv',
+        'prefix': 'asm',
+        'language': 'EN',
+        }
+    }
 
-PRIORITIES = {'Main stage': 10}
+MAPPING_LOCATIONS = {
+    'Big screen': 'MAIN-ARENA',
+    'Stage': 'MAIN-ARENA',
+    }
+
+
+PRIORITIES = {
+    'MAIN-ARENA': 10,
+    'Seminar hall': 9,
+    'Game hall': 7,
+    'PMS': 6,
+    'AssemblyTV': 4,
+    'Infodesk': -1,
+    }
