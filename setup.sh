114cd5073d075f7f5743382802de0e8da048276e
Jussi Judin <jjudin+bitbucket@iki.fi>
2010-04-04 18:16:34 +0300

    Update to Grok 1.1rc1

--- setup.sh
+++ setup.sh
@@ -20,14 +20,12 @@ if test ! -f base.cfg; then
 fi
 
 PYTHON_VERSION="2.5.4"
-SETUPTOOLS_VERSION="0.6c9"
 
 ASMMOBILE_ROOT="`pwd`"
 PACKAGES_ROOT="$ASMMOBILE_ROOT"/packages
 PYTHON_ROOT="$ASMMOBILE_ROOT"/python
 
 PYTHON_PACKAGE="http://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.bz2"
-SETUPTOOLS_PACKAGE="http://pypi.python.org/packages/source/s/setuptools/setuptools-${SETUPTOOLS_VERSION}.tar.gz"
 
 # Install Python
 mkdir -p "$PACKAGES_ROOT"
@@ -38,19 +36,14 @@ cd Python-"${PYTHON_VERSION}"/
 make
 make install
 
-# Install setuptools that provides easy_install
-cd "$PACKAGES_ROOT"
-curl "$SETUPTOOLS_PACKAGE" | tar xvz
-cd setuptools-"$SETUPTOOLS_VERSION"
-"$PYTHON_ROOT"/bin/python setup.py install
-
-# Clean up installation packages
+# Clean up installation packages:
 cd "$ASMMOBILE_ROOT"
 rm -rf "$PACKAGES_ROOT"
 
-# Install zc.buildout
-"$PYTHON_ROOT"/bin/easy_install zc.buildout
+# Bootstrap necessary eggs:
+cd "$ASMMOBILE_ROOT"
+"$PYTHON_ROOT"/bin/python bootstrap.py
 
-# Compile this application
+# Compile this application:
 cd "$ASMMOBILE_ROOT"
 "$PYTHON_ROOT"/bin/buildout
