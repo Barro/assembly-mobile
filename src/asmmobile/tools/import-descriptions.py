fcc2e2ef69c47dc26173f463a203bf411a0f1709
Jussi Judin <jjudin+asmmobile@iki.fi>
2010-08-05 11:15:01 +0300

    Make description importing to handle empty descriptions.

--- src/asmmobile/tools/import-descriptions.py
+++ src/asmmobile/tools/import-descriptions.py
@@ -1,4 +1,4 @@
-def parseData(filename):
+def parseData(filename, defaultLanguage):
     fp = open(filename, "r")
     result = {}
     currentItem = None
@@ -9,18 +9,22 @@ def parseData(filename):
         elif line[0] == '!':
             currentItem = line[1:].strip()
         elif line[0] == ":":
+            # Add space as description can be empty.
+            line += " "
             language, description = (x.strip() for x in line[1:].split(" ", 1))
             if language not in result:
                 result[language] = {}
+            if description == '':
+                description = result[defaultLanguage][currentItem]['description']
             result[language][currentItem] = {'description': description}
     fp.close()
     return result
 
-def importer(locationsFile=None, eventsFile=None):
+def importer(locationsFile=None, eventsFile=None, defaultLanguage='en'):
     locations = {}
     if locationsFile is not None:
-        locations = parseData(locationsFile)
+        locations = parseData(locationsFile, defaultLanguage)
     events = {}
     if eventsFile is not None:
-        events = parseData(eventsFile)
+        events = parseData(eventsFile, defaultLanguage)
     return locations, events
